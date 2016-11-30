41b69f990238a839e6b19aba78f46dfc34e2b410
Mikky88 <mikky.rowe@outlook.com>
2016-11-30 17:08:44 +0100

    Update Oval.java

--- src/geometri/Oval.java
+++ src/geometri/Oval.java
@@ -6,10 +6,12 @@ import java.awt.*;
  * Created by JesperU on 2016-11-16.
  */
 public class Oval extends GeometricalShape{
+    
     private int width;
     private int height;
+   
     /**
-     * Constructor for Oval
+     * Constructor for the Oval.
      * @param x X-coordinate for the left uppermost corner of the narrowest possible Rectangle surrounding the Oval.
      * @param y Y-coordinate for the left uppermost corner of the narrowest possible Rectangle surrounding the Oval.
      * @param width Width of the Oval.
@@ -21,8 +23,9 @@ public class Oval extends GeometricalShape{
         this.width = width;
         this.height = height;
     }
+    
     /**
-     * Constructor for the Oval
+     * Constructor for the Oval.
      * @param f GeometricalForm used for x and y coordinates.
      * @param width Width of the Oval.
      * @param height Height of the Oval.
@@ -32,13 +35,13 @@ public class Oval extends GeometricalShape{
         super(f.getX(), f.getY(), color);
         this.width = width;
         this.height = height;
-        }
-
+    }
+    
     @Override
     public int getArea() {
         return (int)(width * height * Math.PI);
     }
-
+    
     @Override
     public void fill(Graphics g) {
         g.drawOval(this.x, this.y, this.width, this.height);
@@ -53,20 +56,14 @@ public class Oval extends GeometricalShape{
     public int getHeight() {
         return height;
     }
+    
     @Override
     public int getPerimeter() {
         return (int)(2 * Math.PI * Math.sqrt((Math.pow(width, 2) + Math.pow(height, 2)) / 2));
     }
+    
     @Override
     public int hashCode(){
         return 0;
-    }
-    /**
-     * @param o Object for comparison, objects compared by area first and perimeter second.
-     * @return True if all attributes and type is the same, regardless of position.
-     */
-    @Override
-    public boolean equals(Object o){
-        return false;
     }
 }
